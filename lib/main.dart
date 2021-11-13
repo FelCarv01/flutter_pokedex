04fe182e0744bf74bca9edf200706584e0fa1a31
Felipe Carvalho <feliped3carvalho@gmail.com>
2021-11-13 00:54:10 -0300

    init

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_pokedex/common/repositories/pokemon_repository.dart';
+import 'package:flutter_pokedex/features/home/container/home_container.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pokedex_2K21',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: HomeContainer(
+        repository: PokemonRepository(),
+      ),
+    );
+  }
+}
